768d1266f27fe1bacd4950311e9f0cb2c62064ce
Vega Baez Gabriel Omar <15325061080071@cetis108.edu.mx>
2017-06-15 17:12:08 -0600

    Codigos php
    Profesor, tuve que hacer los Push desde la cuenta de Guadalupe hacia mi cuenta porque si no no me dejaba llevar acabo ningún push

---
+++ tema_listar.php
@@ -0,0 +1,25 @@
+<?php 
+$servidor = "localhost";
+$usuario ="15325061080270";
+$pwd='BGG.0270';
+$bd ="4avpr";
+$con = @mysql_connect($servidor, $password) or die ("No se pudo acceder a la base de datos");
+
+mysql_select_db($bd) or die ("error al conectar la base de datos");
+$consulta = "SELECT*
+FROM Tema
+WHERE categoria IN (SELECT categoria)";
+
+$result = mysql_query($consulta, $con);
+$json = array();
+if(mysql_num_rows($result))
+{
+	while ($row = mysql_fetch_object($result)){
+		$json[] = row;
+
+	}
+}
+mysql_close($con);
+echo_json_encodw($json);
+
+?>
