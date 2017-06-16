db92d4638ee06665945dc889afd988460c427ea6
BernalGalvezGuadalupe <15325061080270@cetis108.edu.mx>
2017-06-15 21:06:39 -0700

    Cambio Nombres y funciones

--- app/src/main/java/com/example/gualberto/cetis108app005/Categorias_tipo_Activity.java
+++ app/src/main/java/com/example/gualberto/cetis108app005/Categorias_tipo_Activity.java
@@ -5,17 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Categorias_tipo_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_categorias_tipo_);
     }
-    public void nuevoTema2(View v) {
-        Intent Continuar2 = new Intent (this, Categorias_tipo_Activity.class);
+    public void nuevoTema5(View v) {
+        Intent Continuar2 = new Intent (this, Tema_lista_activity.class);
         startActivity(Continuar2);
     }
-
-
 }
--- app/src/main/java/com/example/gualberto/cetis108app005/Tema_tipo.java
+++ app/src/main/java/com/example/gualberto/cetis108app005/Tema_tipo.java
@@ -5,17 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Tema_tipo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_nuevo);
     }
-    public void nuevoTema2(View v) {
-        Intent Continuar2 = new Intent (this, Categorias_tipo_Activity.class);
-        startActivity(Continuar2);
-    }
-
 
 }
